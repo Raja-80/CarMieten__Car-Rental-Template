2fdd48b0d19256681a113481a0b39b8ddb1407a5
unknown <Raja-80>
2024-02-11 11:07:17 +0100

    Final Changes

--- components/sections/Header.vue
+++ components/sections/Header.vue
@@ -75,7 +75,12 @@
     
     
                     <button @click="toggleMenu" class="text-white p-4" style="z-index: 1000; margin-right: 20px;">
-                        <img class="h-8" src="~/assets/images/icon_menu.png" alt="toggleMenu" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white"
+                            stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <line x1="3" y1="12" x2="21" y2="12"></line>
+                            <line x1="3" y1="6" x2="21" y2="6"></line>
+                            <line x1="3" y1="18" x2="21" y2="18"></line>
+                        </svg>                        
                     </button>
     
                     Responsive menu items
